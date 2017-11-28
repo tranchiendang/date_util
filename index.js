388cf439f159829532a815d1e9df87235e772141
Chien <tranchiendang@gmail.com>
2017-11-28 15:29:16 +0700

    [ADD] add more functions

--- index.js
+++ index.js
@@ -5,9 +5,17 @@ class Util {
 	static get_current_unix_time(){
 		return moment().valueOf();
 	}
+
+	static get_current_datetime(fm){
+		return moment().format(fm);
+	}
 	
 	static get_current_date_unix_time(){
 		return moment().startOf('day').valueOf();
+	}
+
+	static convert_string_to_unix_time(data, fm) {
+		return moment(data, fm).valueOf();
 	}
 	
 	static check_in_range(start, end){
@@ -25,8 +33,8 @@ class Util {
 		return false;
 	}
 
-	static add_datetime(day){
-		return moment().add(day, 'day').format('DD/MM/YYYY HH:mm:ss');
+	static add_datetime(day, fm){
+		return moment().add(day, 'day').format(fm);
 	}
 }
 
